2712ee39320bcfb8a7d796451a4855ad67f4fc3c
Xue Bo <xuebo.com.cn@gmail.com>
2013-09-17 18:31:42 +0800

    expand/root

--- dp-yasnippet.el
+++ dp-yasnippet.el
@@ -7,7 +7,8 @@
 (yas-global-mode t)
 
 ;; Develop and keep my snippets under these
-(setq yas/root-directory '("~/.emacs.d/mysnippets"))
+(setq yas/root-directory 
+      (expand-file-name "/mysnippets" user-emacs-directory))
 
 ;; Map `yas/load-directory' to every element
 (mapc 'yas/load-directory yas/root-directory)
