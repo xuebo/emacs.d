54010d1aa5a3557e58f9f22a36761c88c74ab657
Xue Bo <xuebo.com.cn@gmail.com>
2014-06-15 21:56:23 +0800

    add projectile file-rgrep func

--- dp-init/dp-projectile.el
+++ dp-init/dp-projectile.el
@@ -35,5 +35,25 @@
       (grep-compute-defaults)
       (rgrep search-regexp "*.erl .hrl" root-dir))))
 
+(defun projectile-file-grep (search-regexp file-type dir)
+  "Perform rgrep in the project."
+  (interactive "ssearch string:\nsfile type<*.[php|js]>:\nDdir:")
+  (require 'grep)
+  ;; paths for find-grep should relative and without trailing /
+  (let ((search-regexp
+	 (or search-regexp
+	     (if (and transient-mark-mode mark-active)
+		 (buffer-substring (region-beginning) (region-end))
+	       (read-string
+		(projectile-prepend-project-name
+		 "PhpGrep for: ")
+		(thing-at-point 'symbol)))))
+	(dir (or dir
+		 (expand-file-name (projectile-project-root))))
+	(grep-find-ignored-directories nil)
+	(grep-find-ignored-files nil))
+  (grep-compute-defaults)
+  (rgrep search-regexp file-type dir)))
+
 (provide 'dp-projectile)
 ;;; de-evil.el ends here.
