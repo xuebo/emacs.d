bff1e3e26d98628590177eab6966814441398189
Xue Bo <xuebo.com.cn@gmail.com>
2017-03-23 16:58:43 +0800

    add helm keys

--- dp-init/dp-helm.el
+++ dp-init/dp-helm.el
@@ -2,3 +2,6 @@
 
 (require-package 'helm-package)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h f") 'helm-semantic-or-imenu)
+(global-set-key (kbd "C-c h m") 'helm-man-woman)
